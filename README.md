c477ea7ad9e68537bb6c4186c898a846e3d83031
Kwang Woo Lee <kl467@cornell.edu>
2019-12-04 06:35:47 +0900

    MVP

--- README.md
+++ README.md
@@ -6,49 +6,52 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 **Follow these steps to set up and work on your project:**
 
-* [ ] Create a forked copy of this project.
-* [ ] Add your project manager as collaborator on Github.
-* [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
-* [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
+* [v] Create a forked copy of this project.
+* [v] Add your project manager as collaborator on Github.
+* [v] Clone your OWN version of the repository (Not Lambda's by mistake!).
+* [v] Create a new branch: git checkout -b `<firstName-lastName>`.
+* [v] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+* [v] Push commits: git push origin `<firstName-lastName>`.
 
 **Follow these steps for completing your project.**
 
-* [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
-* [ ] Add your project manager as a reviewer on the pull-request
-* [ ] Your project manager will count the project as complete by merging the branch back into master.
+* [v] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
+* [v] Add your project manager as a reviewer on the pull-request
+* [v] Your project manager will count the project as complete by merging the branch back into master.
 
 ## Task 1: Set Up LESS Preprocessor
 
-* [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
+* [v] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
 
-* [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
+* [v] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
+* [v] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
 
-* [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
+* [v] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [v] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 2: Create Unique Event Listeners
 
-* [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
-	* [ ] `load`
-	* [ ] `focus`
-	* [ ] `resize`
-	* [ ] `scroll`
+* [v] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
+	* [v] `mouseover`
+	* [v] `mouseleave`
+	* [v] `keydown`
+	* [v] `wheel`
+	* [v] `drag / drop`
+	* [v] `load`
+	* [v] `resize`
+	* [v] `scroll`
+	* [v] `dblclick`
+	* [v] `readystatechange`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [ ] `focus`
+
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+* [v] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [v] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,202 @@
 // Your code goes here
+
+// * [v] `mouseover`
+// * [ ] `keydown`
+// * [v] `wheel`
+// * [v] `drag / drop`
+// * [v] `load`
+// * [ ] `focus`
+// * [v] `resize`
+// * [v] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+
+// 1. mouseover
+let logoHeading = document.querySelector('.container .logo-heading');
+logoHeading.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'pink';
+});
+
+// 2. mouseleave
+logoHeading.addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+})
+
+// 3. keydown
+function keyPressed(e) {
+  logoHeading.textContent = logoHeading.textContent + ` ${e.code.substring(3, 5).toLowerCase()}`
+  console.log(e.code);
+}
+document.addEventListener('keydown', keyPressed);
+
+
+// 4. wheel
+let welcome = document.querySelector('.content-destination img');
+welcome.addEventListener('wheel', function(event) {
+    event.target.style.transform = 'scale(1.3)';
+    event.target.style.transition = 'transform 1s';
+})
+
+
+// 5. load
+window.addEventListener('load', event => {
+    console.log('website has been loaded');
+})
+ 
+// 6. resize
+const output = document.querySelector('.text-content h2')
+window.addEventListener('resize', () => {
+    output.textContent = `Width: ${window.innerWidth.toString()}  Height: ${window.innerHeight.toString()}`;
+})
+
+//7. scroll
+let last_known_scroll_position = 0;
+let ticking = false;
+const scrollOutput = document.querySelector('.intro h2');
+
+function doSomething(scroll_position) {
+    // Do something with the scroll position
+    scrollOutput.textContent = `Scroll position: ${scroll_position}`;
+}
+
+window.addEventListener('scroll', event => {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        });
+        ticking = true;
+    }
+})
+
+//8. select
+// function handleSelect(e) {
+//     const selectOutput = document.querySelector('.intro p');
+//     const selection = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd)
+//     console.log(selection)
+//     selectOutput.textContent = `Your selection has ${selection} characters.`;
+// }
+
+// const input = document.querySelector('input');
+// console.log(input);
+// input.addEventListener('select', handleSelect);
+
+// 9. dbclick
+
+const handledbClick = event => {
+    event.target.style.opacity = '0.3';
+    event.target.style.transition = 'opacity 0.6s';
+}
+
+const busImg = document.querySelector('.intro img');
+busImg.addEventListener('dblclick', handledbClick);
+
+// 10. readystatechange
+let loadingHandler = function() {
+    console.log('Second state: this is when loading happens');
+}
+console.log(`First state: ${document.readyState}`);
+if (document.readyState === 'loading') {
+    loadingHandler();
+} else {
+    console.log('Loading complete')
+}
+document.addEventListener('readystatechange', () => {
+    console.log(`Third state: ${document.readyState}, this is after state change`);
+}); 
+
+// 11. drag / drop   ISSUE!!
+let dragImage = document.querySelectorAll('img');
+console.log(dragImage);
+
+dragImage.forEach(el => {
+    el.addEventListener('dragstart', event => {
+        event.target.style.opacity = '0.3';
+    });
+});
+
+
+
+
+// 11. Drag and Drop REFERENCE!
+var dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {
+
+});
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+});
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+});
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+});
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.className == "footer") {
+    event.target.style.background = "purple";
+  }
+
+});
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.className == "footer") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.className == "footer") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
+
+
+
+// stop propagation
+let parent = document.querySelector('.content-section');
+let child = document.querySelector('.text-content');
+
+child.addEventListener('click', event => {
+    event.target.style.background = 'pink';
+    console.log('Child color change');
+    event.stopPropagation();
+})
+
+parent.addEventListener('click', event => {
+    event.target.style.background = 'purple';
+    console.log('Parent color change')
+})
+
+
+// prevent default
+let navItems = document.querySelectorAll('.nav .nav-link');
+navItems.forEach(el => {
+    el.addEventListener('click', event => {
+        event.preventDefault();
+        console.log('prevent default activated')
+    })
+});
